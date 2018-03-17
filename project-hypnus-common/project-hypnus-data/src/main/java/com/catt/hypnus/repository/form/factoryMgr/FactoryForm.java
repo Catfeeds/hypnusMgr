990832cac5f862b577b45a8fe25a2ac70be3a9f2
lyz <liyuzan@96900.com.cn>
2018-03-17 17:36:29 +0800

    TEST

---
+++ project-hypnus-common/project-hypnus-data/src/main/java/com/catt/hypnus/repository/form/factoryMgr/FactoryForm.java
@@ -0,0 +1,19 @@
+package com.catt.hypnus.repository.form.factoryMgr;
+
+/**
+ * @Author:lyz
+ * @Date: 2018/3/17 17:07
+ * @Desc:
+ **/
+public class FactoryForm
+{
+    private String factoryMobile;
+
+    public String getFactoryMobile() {
+        return factoryMobile;
+    }
+
+    public void setFactoryMobile(String factoryMobile) {
+        this.factoryMobile = factoryMobile;
+    }
+}
